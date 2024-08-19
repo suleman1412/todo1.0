2aef161844310a3a56b77d57078e762e3aeb92da
Suleman Karigar <37236131+suleman1412@users.noreply.github.com>
2024-08-19 20:53:14 +0000

    added new functionality to update

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function addTodo(){
   const newEl = document.createElement('div')
   newEl.setAttribute("class", "todos")
   newEl.setAttribute("id", "todo-box-" + ctr)
-
-  // Creating new header and a button which will go under newEl and setting each of their attributes
+  
+  // Creating new components which will go under newEl and setting each of their attributes
   const newHeader = document.createElement('h3')
   newHeader.setAttribute("id", "task")
   if(newTask === ''){
@@ -20,14 +20,22 @@ function addTodo(){
   // Creating delBtn
   const delBtn = document.createElement('button')
   delBtn.setAttribute("id", "deleteBtn")
+  delBtn.setAttribute("class", "additionalBtns")
   delBtn.innerText = 'Delete'
   delBtn.setAttribute("onclick", "delTodo(" + ctr + ")");
+
+  // Creating updBtn
+  const updBtn = document.createElement('button')
+  updBtn.setAttribute("id", "updateBtn")
+  updBtn.setAttribute("class", "additionalBtns")
+  updBtn.innerText = 'Update'
+  updBtn.setAttribute("onclick", "updTodo(" + ctr + ")");
 
 
   // Append all the buttons to newEl
   newEl.appendChild(newHeader)
+  newEl.appendChild(updBtn)
   newEl.appendChild(delBtn)
-  console.log(newEl)
 
   // Append to the body
   const parentEl = document.querySelector('.TopBar')
@@ -41,4 +49,17 @@ function addTodo(){
 function delTodo(index){
   const selectedEl = document.getElementById(`todo-box-${index}`)
   selectedEl.parentNode.removeChild(selectedEl)
-}  
+} 
+
+
+function updTodo(index){
+  const selectedEl = document.getElementById(`todo-box-${index}`)
+  const h3heading = selectedEl.querySelector('h3')
+  const currText = h3heading.textContent
+  const updText = prompt("Enter updated text",currText)
+  if (updText !== ''){
+    h3heading.textContent = updText.trim()
+  }else{
+    alert('Task cannot be empty')
+  }
+}
